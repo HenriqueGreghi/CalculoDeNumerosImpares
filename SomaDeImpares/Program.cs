d6b6633d1ba23f225740045841cb5b232898a683
Henrique Greghi <henriquevsg@gmail.com>
2022-08-24 15:51:26 -0300

    Calculando numeros impares

---
+++ SomaDeImpares/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace SomaDeImpares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int soma = 0;
+
+            Console.WriteLine("Entre com o primeiro valor :");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o segundo valor :");
+            int y = int.Parse(Console.ReadLine());
+           
+            if (x < y)
+            {
+                for (int i = x+1; i < y; i++)
+                { if (i % 2 != 0)
+                    {
+                        soma = soma + i;
+                    }
+
+                }
+            }
+            else if (x > y)
+            {
+                for (int i = y + 1; i < x; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        soma = soma + i;
+                    }
+
+                }
+            }
+            else
+            {
+                
+            }
+            Console.WriteLine("Resultado : " + soma);
+        }
+    }
+}
